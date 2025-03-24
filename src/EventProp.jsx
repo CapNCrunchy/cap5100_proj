37bc24a0284ade9d7300ff6559b8b805c1e89bd6
Connor L <connorlausch@gmail.com>
2025-03-24 19:43:34 -0400

    Create Event Button
    Button now has input boxes for adding onto the events

--- src/EventProp.jsx
+++ src/EventProp.jsx
@@ -1,35 +1,38 @@
 import {useState} from 'react'
+import './EventProp.css'
 
 function EventProp(props){
     const [selected, setSelected] = useState(false);
 
     return(
-        <>
-        <p1>
-            {props.title} 
-        </p1>
-        <p1>
-            {props.start} - {props.end} 
-        </p1>
+        <div class="eventContainer">
+        <div class="infoContainer">
+            <p1 class="info" id="infoTitle">
+                {props.title} {props.start} - {props.end}
+            </p1>
+            <p1 class="info" id="infoLoc">
+                {props.location}
+            </p1>
+        </div>
         
-        <button
+        <button 
             onClick={() => setSelected(!selected)}
-        style={{
-        padding: "10px 20px",
-        fontSize: "16px",
-        borderRadius: "8px",
-        border: "none",
-        cursor: "pointer",
-        backgroundColor: selected ? "#4CAF50" : "#ccc",
-        color: selected ? "white" : "black",
+            style={{
+            padding: "10px 20px",
+            fontSize: "16px",
+            borderRadius: "8px",
+            border: "none",
+            cursor: "pointer",
+            backgroundColor: selected ? "#4CAF50" : "#ccc",
+            color: selected ? "white" : "black",
         }}
         >
         {selected ? "Selected" : "Not Selected"}
         </button>
-        <p1>
-            {props.location}
-        </p1>
-        </>
+
+        
+
+        </div>
     )
 }
 
--- src/Schedule.jsx
+++ src/Schedule.jsx
@@ -2,20 +2,66 @@ import { useState } from 'react'
 import EventProp from "./EventProp"
 
 function Schedule(){
+    const [valTitle,setValTitle]=useState("Title")
+    const [valStart,setValStart]=useState("Start")
+    const [valEnd,setValEnd]=useState("End")
+    const [valLoc,setValLoc]=useState("Location")
+    const [newEventVisible,setNewEventVisible]=useState(false)
+
+    function handleChangeTitle(e){
+        setValTitle(e.target.value)
+    }
+    function handleChangeStart(e){
+        setValStart(e.target.value)
+    }
+    function handleChangeEnd(e){
+        setValEnd(e.target.value)
+    }
+    function handleChangeLocation(e){
+        setValLoc(e.target.value)
+    }
+
+    let titleIn,startIn,endIn,locIn,createButt
+
     let [classEvents, setClassEvents] = useState([
         ["CAP5100", "10:40", "11:30", "NULL"],
         ["CDA5636", "3:00", "4:55", "NULL"],
         ["EEL3111C", "4:05", "4:55", "New Engineering Building"]
     ]);
+
+    if(newEventVisible){
+        titleIn = <input onChange={(e) =>handleChangeTitle(e)} type="text" value={valTitle}/>
+        startIn = <input onChange={(e) =>handleChangeStart(e)} type="text" value={valStart}/>
+        endIn = <input onChange={(e) =>handleChangeEnd(e)} type="text" value={valEnd}/>
+        locIn = <input onChange={(e) =>handleChangeLocation(e)} type="text" value={valLoc}/>
+        createButt = <button onClick={()=>(setClassEvents([...classEvents, [valTitle, valStart, valEnd, valLoc]]),setNewEventVisible(false))}>
+            Create
+        </button>
+    }else{
+        titleIn =<></>
+        startIn =<></>
+        endIn =<></>
+        locIn =<></>
+        createButt=<></>
+    }
     
     return(
         <>
-        <button onClick={() => setClassEvents([...classEvents, ["Empty", "00:00", "00:00", "NULL"]])}>
+        <button onClick={() => setNewEventVisible(true)}>
             Create New Class Event
         </button>
+        <div>
+            {titleIn}
+            {startIn}
+            {endIn}
+            {locIn}
+            {createButt}
+        </div>
+
         <div>{classEvents.map(Ev=><li><EventProp title={Ev[0]} start={Ev[1]} end={Ev[2]} location={Ev[3]}/></li>)}</div>
         </>
     )
 }
 
 export default Schedule
+//setClassEvents([...classEvents, ["Empty", "00:00", "00:00", "NULL"]])
