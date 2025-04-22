459763d4eeeca33bd13eeaf2a9462413e0394078
Strigo Araneo <shanmukhaharshith@gmail.com>
2025-04-22 10:15:31 -0400

    Add map to generate page + css updates

--- vishwath/app.py
+++ vishwath/app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, request, flash, redirect, url_for, ses
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime,timedelta
 import requests
+import pandas as pd
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:SGBR2024@localhost/schedule'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:nasatlx@localhost/schedule'
 #app.config['SQLALCHEMY_DATABASE_URI']='postgresql://flbkyrsudtcdkb:137b6a0124d64919d1a04144413773b6e3d123f34684a96d225f7be6bb29e83c@ec2-54-90-13-87.compute-1.amazonaws.com:5432/dfdvq49i0u2lnr?sslmode=require'
 
 
@@ -18,11 +19,15 @@ import csv
 
 # A dictionary to quickly look up coordinates by location name
 location_coords = {}
+route_start_points = []
 
-with open('static/BuildingInformation.csv', newline='') as csvfile:
+with open("C:/Users/kshar/Desktop/uf/2025_spring/hci/cap5100_proj/vishwath/static/BuildingInformation.csv", newline='') as csvfile:
+
     reader = csv.DictReader(csvfile)
     for row in reader:
         location_coords[row['name']] = (row['latitude'], row['longitude'])
+        if row['name'] and row['latitude'] and row['longitude']:
+            route_start_points.append({'name': row['name'], 'lat': float(row['latitude']), 'lng': float(row['longitude'])})
 
 class Schedule(db.Model):
     __tablename__ = "student"
@@ -76,20 +81,12 @@ def event():
 
 @app.route("/google_map")
 def google_map():
-    route_start_points = [
-        { "name": "Stoneridge Apartments", "lat": 29.620724476508645, "lng": -82.37546282465814 },
-        { "name": "Greenwich Green Apartments", "lat": 29.616989987441865, "lng": -82.37431589666387 },
-        { "name": "BLVD Apartments", "lat": 29.617468210928614, "lng": -82.37767025833176 },
-    ]
-    route_end_points = [
-        { "name": "Reitz Uinon", "lat": 29.6467124109277, "lng": -82.34800087571728 },
-        { "name": "CSE Building", "lat": 29.649290202565588, "lng": -82.3441214802752 },
-        { "name": "MAE Building A", "lat": 29.643444744944144, "lng": -82.34822478920941 },
-    ]
-    return render_template('google_map.html', route_start_points=route_start_points, route_end_points=route_end_points)
+    return render_template('google_map.html', route_start_points=route_start_points)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+    locations_df = pd.read_csv('C:/Users/kshar/Desktop/uf/2025_spring/hci/cap5100_proj/vishwath/static/BuildingInformation.csv')  # Update with your path
+    locations_list = locations_df['name'].tolist()  # Update with your column name
     if request.method == 'POST':
         name = request.form['name']
         start = request.form['start']
@@ -100,7 +97,7 @@ def add():
         db.session.add(schedule)
         db.session.commit()
         return redirect(url_for('schedule'))
-    return render_template('add.html')
+    return render_template('add.html',locations=locations_list)
 
 @app.route("/schedule", methods=['POST','GET'])
 def schedule():
@@ -111,14 +108,20 @@ def schedule():
 def generate():
     schedule = Schedule.query.order_by(Schedule.start).all()
     #put in api here
-    API_KEY =
+    API_KEY = ""
     routes =[]
+    start_coords = []
+    end_coords = []
     for i in range(len(schedule)-1):
         start_loc = schedule[i].loc
         end_loc = schedule[i+1].loc
 
         start_coords = location_coords.get(start_loc)
         end_coords = location_coords.get(end_loc)
+
+        bus_route_origin = {'lat': float(start_coords[0]) , 'lng': float(start_coords[1])} if start_coords else {}
+        bus_route_destination = {'lat': float(end_coords[0]), 'lng': float(end_coords[1])} if end_coords else {}
+
         if start_coords and end_coords:
             origin = f"{start_coords[0]},{start_coords[1]}"
             destination = f"{end_coords[0]},{end_coords[1]}"
@@ -160,10 +163,14 @@ def generate():
         "start_location": schedule[len(schedule)-1].loc,
         "bus_steps": []
     })
-    return render_template('generate.html', schedule=schedule,routes=routes)
+    return render_template('generate.html', schedule=schedule,routes=routes,
+                            route_origin=bus_route_origin,
+                            route_destination=bus_route_destination)
 
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
+    locations_df = pd.read_csv('C:/Users/kshar/Desktop/uf/2025_spring/hci/cap5100_proj/vishwath/static/BuildingInformation.csv')  # Update with your path
+    locations_list = locations_df['name'].tolist()  # Update with your column name
     schedule = Schedule.query.get_or_404(id)
     if request.method == 'POST':
         schedule.name = request.form['name']
@@ -173,7 +180,7 @@ def edit(id):
         schedule.loc = request.form['loc']
         db.session.commit()
         return redirect(url_for('schedule'))
-    return render_template('edit.html', schedule=schedule)
+    return render_template('edit.html', schedule=schedule, locations=locations_list)
 
 @app.route('/delete/<int:id>')
 def delete(id):
